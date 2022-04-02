041cbf25d3e481ccb1075cec10c944f2c8e02380
Joey yang <joeyyang@pcpartner.com>
2022-04-02 18:28:56 +0800

    Feat: part 4 - define your theme
    Solution:
    1. As title.
    
    Author: Joey

---
+++ android-compose-codelabs-main/android-compose-codelabs-main/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
@@ -0,0 +1,41 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val LightColors = lightColors(  //lightColors 要import
+    primary = Red700,
+    primaryVariant = Red900,
+    onPrimary = Color.White,
+    secondary = Red700,
+    secondaryVariant = Red900,
+    onSecondary = Color.White,
+    error = Red800
+)
+
+private val DarkColors = darkColors(  //lightColors 要import
+    primary = Red300,
+    primaryVariant = Red700,
+    onPrimary = Color.Black,    //跟 onSecondary好像要一致
+    secondary = Red300,
+    onSecondary = Color.Black,
+    error = Red200
+)
+
+@Composable
+fun JetnewsTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = if (darkTheme) DarkColors else LightColors,  //先建 LightColors 後,再使用
+        typography = JetnewsTypography,
+        shapes = JetnewsShapes,
+        content = content
+    )
+}
